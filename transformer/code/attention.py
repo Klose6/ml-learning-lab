24aa0304d965f5fed5e510c4376b39a635f294fc
Klose6 <4652730+Klose6@users.noreply.github.com>
2026-09-06 22:00:46 -0700

    add comemnt to the basic attention

--- transformer/code/attention.py
+++ transformer/code/attention.py
@@ -7,18 +7,20 @@ import torch
 seed_size = 42
 torch.manual_seed(seed_size)
 
-# implement the attention formula
+# Implement the attention formula
 X = torch.randn(3, 4)
 Q = X
 K = X
 V = X
+# Calculate the attention scores
 scores = Q@K.T
-
+# Scale
 d_k = K.size(-1)
-
-scaled_scores = scores / d_k ** 0.5
-
+scaled_scores = scores / (d_k ** 0.5)
+# Run softmax on the last dimension(-1) of the tensor
 weights = torch.softmax(scaled_scores, dim=-1)
+# Weighted sum of values
 output = weights@V
+
 print(output)
 print(output.shape)
