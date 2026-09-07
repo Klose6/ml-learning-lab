5f64f77df2d88775e8a07b8e0c924d73a79b3ca1
Klose6 <4652730+Klose6@users.noreply.github.com>
2026-09-07 13:30:28 -0700

    refactor the heads processing into loop

--- transformer/code/attention.py
+++ transformer/code/attention.py
@@ -27,23 +27,22 @@ class MultiHeadAttention(torch.nn.Module):
         Q_heads = torch.split(Q, self.head_dim, dim=-1)
         K_heads = torch.split(K, self.head_dim, dim=-1)
         V_heads = torch.split(V, self.head_dim, dim=-1)
-        # process heads
-        scores_head0 = Q_heads[0] @ K_heads[0].T
-        scores_head0 /= math.sqrt(self.head_dim)
 
-        scores_head1 = Q_heads[1] @ K_heads[1].T
-        scores_head1 /= math.sqrt(self.head_dim)
-        # causal mask
-        mask_head0 = torch.triu(torch.ones(scores_head0.shape, dtype=torch.bool), diagonal=1)
-        scores_head0 = scores_head0.mask_fill(mask_head0, float("-inf"))
-        mask_head1 = torch.triu(torch.ones(scores_head1.shape, dtype=torch.bool), diagonal=1)
-        scores_head1 = scores_head1.mask_fill(mask_head1, float("-inf"))
-        # softmax
-        weights_head0 = torch.softmax(scores_head0, dim=-1)
-        weights_head1 = torch.softmax(scores_head1, dim=-1)
-        # output
-        output_head0 = weights_head0 @ V_heads[0]
-        output_head1 = weights_head1 @ V_heads[1]
-        output = torch.cat([output_head0, output_head1], dim=-1)
+        outputs = []
+        # Processing the multiple heads
+        for q, k, v in zip(Q_heads, K_heads, V_heads):
+            # process heads
+            scores = q @ k.T
+            scores /= math.sqrt(self.head_dim)
+            # causal mask
+            mask = torch.triu(torch.ones(scores.shape, dtype=torch.bool), diagonal=1)
+            scores = scores.mask_fill(mask, float("-inf"))
+            # softmax
+            weights= torch.softmax(scores, dim=-1)
+            # output
+            output_head = weights @ v
+            outputs.append(output_head)
+
+        output = torch.cat(outputs, dim=-1)
         output = self.output_project(output)
         return output
